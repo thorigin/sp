1cfd2bef199041c16de09aa505922e6853465886
Omar Thor <omarthoro@gmail.com>
2018-02-18 13:24:34 -0800

    ds-feature initial commit

---
+++ include/sp/util/dataset/data_reader.hpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) Omar Thor <omarthoro@gmail.com> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Omar Thor <omarthoro@gmail.com>, 2017
+ */
+
+#ifndef SP_UTIL_QUERY_HPP
+#define SP_UTIL_QUERY_HPP
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+SP_UTIL_NAMESPACE_BEGIN
+
+struct data_reader {
+    
+};
+
+SP_UTIL_NAMESPACE_END
+
+
+#endif /* SP_UTIL_QUERY_HPP */
