50d709374d6dde94357d9d5337051fb825e26b36
Omar Thor <omarthoro@gmail.com>
2018-02-18 13:24:34 -0800

    ds-feature initial commit

---
+++ include/sp/util/dataset.hpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) Omar Thor <omarthoro@gmail.com> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Omar Thor <omarthoro@gmail.com>, 2017
+ */
+
+#ifndef SP_UTIL_DATASET_HPP
+#define	SP_UTIL_DATASET_HPP
+
+#include "dataset/data_reader.hpp"
+
+#endif /* SP_UTIL_DATASET_HPP */
