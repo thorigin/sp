571151c081bef9541dd37b433e98ba76ac17860a
Jacob Escobedo <Jescobedo1901@gmail.com>
2018-02-18 18:04:54 -0800

    Read MNIST data set by frames

--- include/sp/util/dataset/mnist_data_reader.hpp
+++ include/sp/util/dataset/mnist_data_reader.hpp
@@ -24,10 +24,19 @@ SP_UTIL_NAMESPACE_BEGIN
 namespace fs = boost::filesystem;
 
 struct mnist_data_reader  {
-
-    mnist_data_reader(const std::string& image_file, const std::string& labels_file)
-        : image_stream(), label_stream(), image_count(0), image_height(), image_width() {
-
+    
+    const static size_t default_samples_read = 1;
+    
+    
+    using vec_t = std::vector<uint8_t>;
+    using mat_t = std::vector<vec_t>;
+    using tensor_t = std::vector<mat_t>;
+    
+    using result_type = tensor_t;
+    
+    mnist_data_reader(const std::string& image_file, const std::string& labels_file, size_t batch_size = default_samples_read)
+        : image_stream(), label_stream(), image_count(0), image_height(), image_width(), batch_size(batch_size), current_index(0) {
+      
         //read file
         fs::path image_path(image_file);
         fs::path labels_path(labels_file);
@@ -61,11 +70,14 @@ struct mnist_data_reader  {
                 throw std::runtime_error("Failed reading header");
             }
 
+            if(image_count % batch_size != 0) {
+                throw std::runtime_error("Batch size should be a factor of sample size");
+            }
+            
         } else {
             throw std::runtime_error("Image or label file missing");
         }
     }
-
 
     size_t size() {
         return image_count;
@@ -75,7 +87,42 @@ struct mnist_data_reader  {
         image_stream.close();
         label_stream.close();
     }
+    
+    size_t frame_size() {
+        return batch_size;
+    }
+    
+    size_t frame_width() {
+        return image_width;
+    }
+    
+    size_t frame_height() {
+        return image_height;
+    }
+    
+    bool has_next_frame() {
+        return current_index < image_count;
+    }
 
+    result_type read_frame() {
+        //batch_sized
+        result_type result;
+        for(size_t si = 0; si < batch_size; ++si) {
+            mat_t m;
+            m.reserve(image_height);
+            for (size_t hi = 0; hi < image_height; hi++) {
+                vec_t v;
+                v.reserve(image_width);
+                for (size_t wi = 0; wi < image_width; wi++) {
+                    v.emplace_back(read_stream<int32_t>(image_stream));
+                }
+                m.emplace_back(std::move(v));
+            }
+            result.emplace_back(std::move(m));
+        }
+        current_index += batch_size;
+        return result;
+    }    
 private:
 
     template<typename T>
@@ -90,6 +137,8 @@ private:
     uint32_t image_count;
     uint32_t image_height;
     uint32_t image_width;
+    size_t batch_size;
+    size_t current_index;
 
 };
 
